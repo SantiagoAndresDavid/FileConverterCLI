70f21c8bd08f0fc9972666aa4d940f16e95cea31
Santiago Andres David Gomez <sadavid@unicesar.edu.co>
2026-08-08 11:39:40 -0500

    refactor terraform structure, and create resource group

---
+++ jenkins/terraform/environments/dev/main.tf
@@ -0,0 +1,14 @@
+module "resource_group" {
+
+  source = "../../modules/resource_group"
+
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Project     = "jenkins"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+
+}
