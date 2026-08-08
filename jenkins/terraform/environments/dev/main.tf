86aff28fc12c8501f4f1631b7bef048dc4f68916
Santiago Andres David Gomez <sadavid@unicesar.edu.co>
2026-08-08 14:43:59 -0500

    add new module network: create Virtual network and subnets

--- jenkins/terraform/environments/dev/main.tf
+++ jenkins/terraform/environments/dev/main.tf
@@ -12,3 +12,25 @@ module "resource_group" {
   }
 
 }
+
+
+module "network" {
+  source = "../../modules/network"
+
+  resource_group_name = module.resource_group.name
+  location            = var.location
+
+  vnet_name = var.vnet_name
+
+  vnet_address_space = var.vnet_address_space
+
+  subnet_name = var.subnet_name
+
+  subnet_address_prefixes = var.subnet_address_prefixes
+
+  tags = {
+    Project     = "jenkins"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
--- jenkins/terraform/environments/dev/variables.tf
+++ jenkins/terraform/environments/dev/variables.tf
@@ -1,7 +1,29 @@
 variable "resource_group_name" {
-  type = string
+  description = "Nombre del Resource Group"
+  type        = string
 }
 
 variable "location" {
-  type = string
+  description = "Región de Azure"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Nombre de la Virtual Network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space de la Virtual Network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Nombre de la subnet"
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes de la subnet"
+  type        = list(string)
 }
